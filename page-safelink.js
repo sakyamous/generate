281d97603f29922505989737f8bdc511802b795e
sakyamous <74072188+sakyamous@users.noreply.github.com>
2020-11-17 00:00:19 +0700

    Update page-safelink.js

--- page-safelink.js
+++ page-safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://ig.infomediaku.net/page-safelink/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://ig.infomediaku.net/happy-child-jumping-with-colorful-toy-balloons/?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
